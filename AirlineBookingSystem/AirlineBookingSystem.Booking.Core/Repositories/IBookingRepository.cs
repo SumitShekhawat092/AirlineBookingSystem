323aa97474a71581a3f58e7f245a3b20110b030d
SumitShekhawat092 <106969952+SumitShekhawat092@users.noreply.github.com>
2025-08-15 11:46:45 +0530

    Create Microservice Based Initial Project Structure for Services (Booking, Flight, Notification and Payment)
    - Add Git-ignore File
    - Create Airline Service Projects like Booking, Flight, Notification and Payment
    - Add Entities into Core Projects for all Service Projects
    - Add Repositories into Core Projects for all Service Projects

---
+++ AirlineBookingSystem/AirlineBookingSystem.Booking.Core/Repositories/IBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirlineBookingSystem.Bookings.Core.Entities;
+
+namespace AirlineBookingSystem.Bookings.Core.Repositories
+{
+    public interface IBookingRepository
+    {
+        Task<Booking> GetBookingByIdAsync(int id);
+        Task AddBookingAsync(Booking booking);
+    }
+}
